7ed8dfe66654159c07e3bb06f344e06fb7318f0f
Les Matheson <les.matheson@gmail.com>
2022-09-06 20:51:00 -0400

    Add taskrc for .devcontainer

---
+++ .devcontainer/taskrc.mk
@@ -0,0 +1,51 @@
+# taskrc.mk for .devcontainer
+#
+#  TODO: add targets for your project here.  When you run
+#  "tmk <target-name>", the current dir will not change but
+#  this makefile will be invoked.
+
+
+# See https://stackoverflow.com/a/73509979/237059
+absdir:=$(taskrc_dir)
+
+REMAKE := $(MAKE) -f $(lastword $(MAKEFILE_LIST))
+
+container_name = shellkit-dev
+
+.PHONY: help
+help:
+	@echo "Targets in $(basename $(lastword $(MAKEFILE_LIST))):" >&2
+	@$(REMAKE) -s --print-data-base --question no-such-target 2>/dev/null | \
+	grep -Ev  -e '^taskrc.mk' -e '^help' -e '^(Makefile|GNUmakefile|makefile|no-such-target)' | \
+	awk '/^[^.%][-A-Za-z0-9_]*:/ \
+			{ print substr($$1, 1, length($$1)-1) }' | \
+	sort | \
+	pr --omit-pagination --width=100 --columns=3
+
+.PHONY: my-target-1
+my-target-1:
+	@echo "Hello my-target-1, PWD=$$PWD, taskrc_dir=$$taskrc_dir"
+
+.semaphore/shellkit-test-base: Dockerfile taskrc.mk
+	docker pull ubuntu
+	docker tag ubuntu shellkit-test-base:latest
+	touch .semaphore/shellkit-test-base
+
+.PHONY: dc-up 
+dc-up: .semaphore/shellkit-test-base
+	docker-compose up
+	touch .semaphore/shellkit-test-base
+
+.PHONY: dc-down
+dc-down:
+	docker-compose down
+
+.PHONY: dc-shell
+dc-shell:
+	docker-compose exec -w /workspace -u vscode $(container_name) bash
+
+.PHONY: clean
+clean:
+	rm .semaphore/*
+
+
