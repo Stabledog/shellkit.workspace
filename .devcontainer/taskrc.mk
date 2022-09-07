3ee96bb0359949d3a0acf807385eee889458a832
Les Matheson <les.matheson@gmail.com>
2022-09-07 11:16:36 +0000

    sync

--- .devcontainer/taskrc.mk
+++ .devcontainer/taskrc.mk
@@ -1,51 +1,57 @@
-# taskrc.mk for .devcontainer
+# taskrc.mk for shellkit.workspace/.devcontainer
 #
-#  TODO: add targets for your project here.  When you run
-#  "tmk <target-name>", the current dir will not change but
-#  this makefile will be invoked.
 
 
-# See https://stackoverflow.com/a/73509979/237059
 absdir:=$(taskrc_dir)
-
-REMAKE := $(MAKE) -f $(lastword $(MAKEFILE_LIST))
-
+REMAKE := $(MAKE) -C $(absdir) -s -f $(lastword $(MAKEFILE_LIST))
 container_name = shellkit-dev
 
 .PHONY: help
 help:
 	@echo "Targets in $(basename $(lastword $(MAKEFILE_LIST))):" >&2
-	@$(REMAKE) -s --print-data-base --question no-such-target 2>/dev/null | \
+	@$(REMAKE) --print-data-base --question no-such-target 2>/dev/null | \
 	grep -Ev  -e '^taskrc.mk' -e '^help' -e '^(Makefile|GNUmakefile|makefile|no-such-target)' | \
 	awk '/^[^.%][-A-Za-z0-9_]*:/ \
 			{ print substr($$1, 1, length($$1)-1) }' | \
 	sort | \
 	pr --omit-pagination --width=100 --columns=3
+	@echo "taskrc_dir=$(taskrc_dir)"
+	@echo "CURDIR=$(CURDIR)"
 
-.PHONY: my-target-1
-my-target-1:
-	@echo "Hello my-target-1, PWD=$$PWD, taskrc_dir=$$taskrc_dir"
-
-.semaphore/shellkit-test-base: Dockerfile taskrc.mk
+.PHONY: shellkit-test-base
+shellkit-test-base .semaphore/shellkit-test-base: Dockerfile
 	docker pull ubuntu
 	docker tag ubuntu shellkit-test-base:latest
 	touch .semaphore/shellkit-test-base
 
-.PHONY: dc-up 
-dc-up: .semaphore/shellkit-test-base
+.PHONY: shellkit-test-vsudo
+shellkit-test-vsudo .semaphore/shellkit-test-vsudo: .semaphore/shellkit-test-base
+	@# Base image with just vscode-user + sudo powers
+	docker build --target vsudo-base -t shellkit-test-vsudo:latest . \
+	&& echo "shellkit-test-vsudo:latest image built OK" >&2
+	touch .semaphore/shellkit-test-vsudo
+
+.PHONY: shellkit-test-withtools
+shellkit-test-withtools .semaphore/shellkit-test-withtools: .semaphore/shellkit-test-vsudo
+	@# Vsudo image with basic maintenance tools (git, curl, make)
+	docker build --target withtools -t shellkit-test-withtools:latest . \
+	&& echo "shellkit-test-withtools image built OK" >&2
+	touch .semaphore/shellkit-test-withtools
+
+.PHONY: dc-up
+dc-up .semaphore/dc-up: .semaphore/shellkit-test-base
 	docker-compose up
-	touch .semaphore/shellkit-test-base
+	touch .semaphore/dc-up
 
 .PHONY: dc-down
 dc-down:
 	docker-compose down
+	rm .semaphore/dc-down
 
 .PHONY: dc-shell
-dc-shell:
+dc-shell: .semaphore/dc-up
 	docker-compose exec -w /workspace -u vscode $(container_name) bash
 
 .PHONY: clean
 clean:
 	rm .semaphore/*
-
-
