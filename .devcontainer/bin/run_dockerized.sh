4fe53e32a69a609d9e95d1888d16c601bcf57707
Les Matheson <lmatheson4@bloomberg.net>
2022-11-16 23:50:56 +0000

    dockins improvements

--- .devcontainer/bin/run_dockerized.sh
+++ .devcontainer/bin/run_dockerized.sh
@@ -7,19 +7,28 @@ die() {
     exit 1
 }
 
+PS4='\033[0;33m+(${BASH_SOURCE}:${LINENO}):\033[0m ${FUNCNAME[0]:+${FUNCNAME[0]}(): }'
+
 set -x
 
 ( sudo chown $(id -u):$(id -u) /var/run/docker.sock || die "Failed changing ownership of /var/run/docker.sock" )
 ( make setup-workspace || die "make setup-workspace failed in $PWD"; )
 ( # Bash history preservation:
-    [[ -d /vdata ] && {
+    [[ -d /vdata ]] && {
         mkdir -p /vdata/home
-        [[ -f /vdata/home/.bash_history ]] && {
-            ln -sf ~/.bash_history /vdata/home/
-        } || {
-            touch ~/.bash_history
-            mv ~/.bash_history /vdata/home/ && ln -sf /vdata/home/.bash_history ~/
+        # We want ~/.bash_history to always be a symlink to /vdata/home
+        [[ -L /vdata/home/.bash_history ]] && {
+            rm /vdata/home/.bash_history
         }
+        [[ -L ~/.bash_history ]] || {
+            ln -sf /vdata/home/.bash_history ~/.bash_history
+        }
+        [[ -f /vdata/home/.bash_history ]] || {
+            touch /vdata/home/.bash_history
+        }
+        HISTFILE=~/.bash_history
+        history -s "run_dockerized.sh - history init"
+        history -w
     }
 )
 
