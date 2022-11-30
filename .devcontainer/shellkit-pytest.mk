3860281695f90636435fcdd53dc5e65661cc4385
Les Matheson <lmatheson4@bloomberg.net>
2022-11-30 15:43:59 -0500

    Add run target

--- .devcontainer/shellkit-pytest.mk
+++ .devcontainer/shellkit-pytest.mk
@@ -4,7 +4,18 @@ Component=shellkit-pytest
 SHELL=/bin/bash
 Baseimg=$(shell bin/get_metabase.sh $(Component) )
 
+
 .PHONY: image
 image:
-	docker build -t $(Component) - \
-		< <( sed -e "s|<component-name>|$(Component)|" -e "s|<base-image-name>|$(Baseimg)|" $(Component).dockerfile )
+	sed \
+		-e "s|<component-name>|$(Component)|" \
+		-e "s|<base-image-name>|$(Baseimg)|" \
+		$(Component).dockerfile \
+		|  \
+		BUILDKIT_PROGRESS=plain docker build -t $(Component) -
+
+.PHONY: run
+run:
+	@docker run $$Volumes --rm -it $(Component):latest $$Cmdline
+
+
